64f5ccbeaa0362ee399b8c128c1348aabf23c355
wonheep <wonheeprk@gmail.com>
2018-03-11 17:29:52 -0700

    in progress, convert given json to d3 so d3 json

---
+++ js/json_d3_parser.js
@@ -0,0 +1,30 @@
+var input = '[{"Great Britain":[{"predicate":{"type":"uri","value":"http://schema.org/spatialCoverage"},"subject":{"type":"uri","value":"http://estc.bl.uk/N72338"},"object":{"type":"literal","value":"Great Britain"}},{"predicate":{"type":"uri","value":"http://schema.org/spatialCoverage"},"subject":{"type":"uri","value":"http://estc.bl.uk/N72335"},"object":{"type":"literal","value":"Great Britain"}}]}]'
+
+var links = [];
+var nodes = [];
+
+for (var title in input) {
+	for (var variation in title) {
+		for (var triple in variation) {
+			var triple_type = variation[triple];
+			var nodes_type = variation[triple];
+			if (triple_type == "subject") {
+				triple_type.source = variation[triple].value;
+				nodes_type.id = variation[triple].value;
+			}
+			if (triple_type == "object") {
+				triple_type.target = variation[triple].value;
+				nodes_type.id = variation[triple].value;
+			}
+			triple_type.value = 1;
+			nodes_type.group = 1;
+			links.push(triple_type);
+			nodes.push(nodes_type);
+		}
+	}
+}
+
+//var obj = JSON.parse(responseString);
+
+console.log(links);
+console.log(nodes);
